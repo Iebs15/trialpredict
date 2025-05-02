ea3c232e47a031ccb9c04a41e173f210f9c1e966
Iebs15 <ayush.kumar@iebrain.com>
2025-05-02 17:33:07 +0530

    okokok

--- backend/app.py
+++ backend/app.py
@@ -148,4 +148,4 @@ def search_drugs():
         return jsonify({"error": str(e)}), 500
 # Run the app
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=6000)
+    app.run(host="0.0.0.0", port=8000)
